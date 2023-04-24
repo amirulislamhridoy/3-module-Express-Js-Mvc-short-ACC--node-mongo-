6d504894dd2aa8225e7e19b171aa057f4d3f294c
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2023-04-24 22:34:07 +0600

    db error handle

--- 3-mvc-pattern/index.js
+++ 3-mvc-pattern/index.js
@@ -2,29 +2,13 @@ const express = require('express')
 const toolsRouter = require('./router/v1/tools.router')
 const app = express()
 const port = process.env.PORT || 5000
+require('./app')
 var cors = require('cors')
 const { viewCount } = require('./middleware/viewCounts')
 const { limiter } = require('./middleware/limitter')
-
-// middlewares
-// core middler
-app.use(cors())
-// building middleware
-app.use(express.json())
-// application label middleware
-// app.use(viewCount)
-// router label middleware => now in tools.counter
-// third parth middleware
-// app.use(limiter)
-
-// send static files
-app.use(express.static('public'))
-
-// view engine
-app.set("view engine", "ejs");
+const { errorHandle } = require('./middleware/errorHandle')
 
 app.use('/api/v1', toolsRouter)
-
 app.get('/', (req, res) => {
     // res.send('Hello world')
     // ejs file all time views file a access thake tai view file a sudu name ta dile i hobe
@@ -35,6 +19,17 @@ app.all('*', (req, res, next) => {
     res.send("Not Found | 404")
 })
 
+// errorHandle middleware
+app.use(errorHandle)
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+// error ja express handle korte partese na(jamon database related error)
+process.on("unhandledRejection", (error) => {
+  console.log(error.name, error.message)
+  app.close(() => {
+    process.exit(1)
+  })
 })
---
+++ 3-mvc-pattern/middleware/errorHandle.js
@@ -0,0 +1,5 @@
+const errorHandle = (error, req, res, next) => {
+    console.log('error handle function | middleware')
+}
+
+module.exports = {errorHandle}
